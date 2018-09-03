7c8856a95f3a53a48f5e63b6e0de1243606df029
Denise Loise Concio <Denise Concio@egg.ph>
2018-09-03 14:39:52 +0800

    initial commit

---
+++ Scripts/sample/Script1533024866376.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+
+Mobile.comment('Loading Screen')
+Mobile.delay(5)
+Mobile.startApplication('C:\\Users\\denise concio\\Documents\\Appletini\\Android APKs\\app-debug (1).apk', false)
+Mobile.delay(5)
+//Mobile.verifyElementVisible(findTestObject('Appletini_Android/android.widget.TextView2 - Loading...'), 5)
+//Mobile.delay(5)
+//Mobile.comment('Loading message should appear.')
+Mobile.delay(5)
+Mobile.tap(findTestObject('Appletini_Android/android.view.View30 - Skip'), 5)
+Mobile.delay(5)
+Mobile.closeApplication()
+
+
